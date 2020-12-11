0d9e94a8701fac72aa5c53856ea5312b243638af
Vasileios Koutsokostas <basilhskk@users.noreply.github.com>
2020-12-12 01:19:52 +0200

    adonisJs framework added for the c2 api&dashboard

---
+++ c2-server/app/Controllers/Http/Auth/RegisterController.js
@@ -0,0 +1,33 @@
+'use strict'
+
+class RegisterController {
+    async register({request, session, response,view}){
+        // console.log(request.all())
+        // const validation = await validate(request.all(),{
+        //     username: 'required|unique:users,username',
+        //     email: 'required|email|unique:users,email',
+        //     password: 'required',
+        // })
+        // let err= "Sorry, username or email is already in use!"
+        // console.log(validation.fails())
+        // if(validation.fails()){
+        //     return view.render('auth.register',{err:err})
+        // }
+
+        const newuser = new User()
+
+            newuser.username= "test"
+            newuser.email= "test@example.com"
+            newuser.password= "123456"
+
+        await newuser.save()
+        
+        return "ok"
+        // response.redirect('/login')
+
+  
+
+    }
+}
+
+module.exports = RegisterController
