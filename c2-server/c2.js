a98a607e0eed90f6640ac2a2a803e6555e134e09
Vasileios Koutsokostas <basilhskk@users.noreply.github.com>
2020-12-14 17:16:30 +0200

    added server status

--- c2-server/c2.js
+++ c2-server/c2.js
@@ -2,6 +2,7 @@ const express = require('express')
 const axios = require('axios');
 const app = express()
 const port = 39943
+var os = require('os');
 
 app.post('/', (req, res) => {
   res.send('Hello World!')
@@ -30,6 +31,25 @@ app.post('/test', (req, res) => {
     res.send('Hello World!')    
 })
   
+// server status infos
+app.get('/server-status', (res) => {
+
+  let servStatus = {
+    "hostname": os.hostname(),
+    "uptime":os.uptime(),
+    "totalmem":os.totalmem(),
+    "freemem":os.freemem(),
+    "netwroks":os.networkInterfaces(),
+    "cpus":os.cpus(),
+    "platfrom":os.platform(),
+    "release":os.release(),
+    "user":os.userInfo(),
+    "arch":os.arch(),
+    "type":os.type()
+  }
+
+  res.send(servStatus)
+})
 
 // run app on public 
 app.listen(port, '0.0.0.0');
